62e24bc194a5ff88e6047bc5e83c1a315ce09278
tanzilmia <ziltan590@gmail.com>
2022-11-08 21:08:29 +0600

    send dynamic data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,13 @@ async function run(){
         const cursor = photographServices.find(query);
         const services = await cursor.toArray()
         res.send(services)
+     })
+
+     app.get('/services:id', async (req,res)=>{
+        const id = req.params.id
+        const query = { _id:ObjectId(id)}
+        const service = await photographServices.findOne(query)
+        res.send(service)
      })
 
      
