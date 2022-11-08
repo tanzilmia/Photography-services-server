332a97d785b0791b26fd55d66a3026b248573db6
tanzilmia <ziltan590@gmail.com>
2022-11-08 21:43:08 +0600

    vercel add

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nz3kcdw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//   const photographyDB = client.db("DreamPhotoGraphy").collection("Services");
-
 async function run(){
    try{
     const photographServices = client.db("DreamPhotoGraphy").collection("Services");
