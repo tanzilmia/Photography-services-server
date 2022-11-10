da01a1ea86c77348e5fb5f52dbba33952dd58e0e
tanzilmia <ziltan590@gmail.com>
2022-11-10 10:07:53 +0600

    jwt token add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -14,6 +15,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nz3kcdw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWt (req,res,next){
+  const authHeader = req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).send({message: 'unauthorized Access'})
+
+    }
+    const token = authHeader.split(' ')[1] 
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err,decoded){
+      if(err){
+        return res.status(403).send({message: 'forbidden Access'})
+      }
+      req.decoded = decoded 
+      next() 
+    })
+}
+
+
 async function run(){
    try{
     const photographServices = client.db("DreamPhotoGraphy").collection("Services");
@@ -25,6 +44,14 @@ async function run(){
       res.send(result)
     })
 
+
+    // post token 
+
+    app.post('/jwt', (req,res)=>{   
+      const user = req.body 
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN, {expiresIn:'1h'})
+      res.send({token}) 
+    })
 
     app.get('/userreview', async (req,res)=>{
       let query = {}
@@ -42,7 +69,11 @@ async function run(){
 
 
 
-    app.get('/myreviw', async (req,res)=>{
+    app.get('/myreviw', verifyJWt,  async (req,res)=>{
+      if(req.decoded.email !== req.query.email){
+       return  res.status(403).send({message : 'unauthorized access' })
+      }
+
       let query = {}
       if(req.query.email){
          query = {
